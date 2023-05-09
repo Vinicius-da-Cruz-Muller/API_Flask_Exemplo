2107687485f6a3ef1e87e191b89671a4bb6ee748
Vinicius-da-Cruz-Muller <108363297+Vinicius-da-Cruz-Muller@users.noreply.github.com>
2023-05-08 21:08:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,38 @@ livros = [
     },
 ]
 #Consultar todos
+@app.route('/livros', methods=['GET']) #@app.route('/livros') funciona, mas é preciso especificar que só poderemos usar esse método - só ele será aceito
+def obter_livros():
+    return jsonify(livros)
+ #http://localhost:5000\livros para ver os livros no navegador
 #Consultar por id
+@app.route('/livros/<int:id>', methods=['GET']) #http://localhost:5000\livros\1 por exemplo
+def obter_livro_por_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+        
 #Editar
+@app.route('/livros/<int:id>', methods=['PUT']) #No postman - PUT, Body, raw, text para json, cola alteração, especifica id no endereço localhost
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+#Criar
+@app.route('/livros', methods=['POST']) #POST, Body, raw, text para json, inclui alteração, endereço localhost (sem id, o id é incluso na alteração. não repetir ids)
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+
+    return jsonify(livros)
 #Excluir
+@app.route('/livros<int:id>', methods=['DELETE'])# altera para delete e passa o id
+def excluir_livro(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+
+    return jsonify(livros)
+app.run(port=5000, host='localhost', debug=True)
